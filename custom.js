c96fa888f22c303cfae90b8e8df582a1da96e039
yukii22-bit <arcadeyuki02@gmail.com>
2026-02-19 02:56:18 +0600

    Add custom JavaScript for navbar and background toggle

---
+++ custom.js
@@ -0,0 +1,74 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height();
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+
+    $(window).on('scroll', function(){
+      function isScrollIntoView(elem, index) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(window).height()*.5;
+        if(elemBottom <= docViewBottom && elemTop >= docViewTop) {
+          $(elem).addClass('active');
+        }
+        if(!(elemBottom <= docViewBottom)) {
+          $(elem).removeClass('active');
+        }
+        var MainTimelineContainer = $('#vertical-scrollable-timeline')[0];
+        var MainTimelineContainerBottom = MainTimelineContainer.getBoundingClientRect().bottom - $(window).height()*.5;
+        $(MainTimelineContainer).find('.inner').css('height',MainTimelineContainerBottom+'px');
+      }
+      var timeline = $('#vertical-scrollable-timeline li');
+      Array.from(timeline).forEach(isScrollIntoView);
+    });
+  
+  })(window.jQuery);
+
+
+// Add to your custom.js file
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleBtn = document.getElementById('bg-toggle');
+  const heroSection = document.querySelector('.hero-section');
+  
+  toggleBtn.addEventListener('click', function() {
+    heroSection.classList.toggle('bg-hidden');
+    
+    // Update button icon/text
+    const icon = this.querySelector('i');
+    if (heroSection.classList.contains('bg-hidden')) {
+      icon.classList.remove('bi-image');
+      icon.classList.add('bi-layout-sidebar');
+      this.innerHTML = '<i class="bi-layout-sidebar"></i> Show Background';
+    } else {
+      icon.classList.remove('bi-layout-sidebar');
+      icon.classList.add('bi-image');
+      this.innerHTML = '<i class="bi-image"></i> Hide Background';
+    }
+  });
+});
